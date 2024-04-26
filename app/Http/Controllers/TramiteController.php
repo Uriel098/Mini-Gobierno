4720a6814312a26670e6469400b4f61991042b9b
Uriel098 <a22311064@uthermosiilo.edu.mx>
2024-04-25 21:33:59 -0700

    ya quedo un controller 1

--- app/Http/Controllers/TramiteController.php
+++ app/Http/Controllers/TramiteController.php
@@ -6,5 +6,63 @@ use Illuminate\Http\Request;
 
 class TramiteController extends Controller
 {
-    //
+    public function index()
+    {
+        $tramites = Store::all();
+        return view('tramites.tramites', compact('tramites')); 
+    }
+
+    public function show($id)
+    {
+        return view('tramites.show');
+    }
+
+    public function TramitesCrear()
+    {
+        return view('tramites.createtramites');
+    }
+
+    public function TramiteCreate(Request $request)
+    {
+        $tramite = new Tramite();
+        $tramite->name = $request->name;
+        $tramite->precio = $request->precio;
+        $tramite->status = $request->status;
+        $tramite->save();
+
+        return redirect()->route('tramites.tramites');
+    }
+
+    public function TramitesEditar($id)
+    {
+        $tramite = Tramite::find($id);
+        return view('tramites.EditTramites', compact('tramite'));
+    }
+
+    public function TramitesEdit(Request $request, $id)
+    {
+        $tramite = Tramite::find($id);
+        $tramite->name = $request->name;
+        $tramite->precio = $request->precio;
+        $tramite->status = $request->status;
+        $tramite->save();
+
+        return redirect()->route('tramites.tramites');
+    }
+
+    public function TramitesDelete($id)
+    {
+        $tramite = Tramite::find($id);
+        $tramite->delete();
+
+        return redirect()->route('tramites.tramites');
+    }
+    
+    public function search(Request $request)
+    {
+        $search = $request->get('search');
+        $tramites = Tramite::where('name', 'like', '%'.$search.'%')->paginate(5);
+        return view('tramites.tramites', compact('tramites'));
+    }
+
 }
