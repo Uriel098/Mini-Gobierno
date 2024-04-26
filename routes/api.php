0aed5aef952058538065d94f452a3ab81c31c1e1
Uriel098 <a22311064@uthermosiilo.edu.mx>
2024-04-25 20:51:09 -0700

    ya quedaron las apis

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TramiteController;
+use App\Http\Controllers\TipoController;
+use App\Http\Controllers\DependenciaController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// api de Tramites
+Route::get ('/Tramites', [TramiteController::class, 'list']);
+Route::get ('/Tramites/{id}', [TramiteController::class, 'item']);
+Route::post ('/Tramites/create', [TramiteController::class, 'create']);
+Route::post ('/Tramites/update', [TramiteController::class, 'update']);
+Route::delete ('/Tramites/{id}', [TramiteController::class, 'delete']);
+
+// api de Tipos
+Route::get ('/Tipos', [TipoController::class, 'list']);
+Route::get ('/Tipos/{id}', [TipoController::class, 'item']);
+Route::post ('/Tipos/create', [TipoController::class, 'create']);
+Route::post ('/Tipos/update', [TipoController::class, 'update']);
+Route::delete ('/Tipos/{id}', [TipoController::class, 'delete']);
+
+// api de Dependencias
+Route::get ('/Dependencias', [DependenciaController::class, 'list']);
+Route::get ('/Dependencias/{id}', [DependenciaController::class, 'item']);
+Route::post ('/Dependencias/create', [DependenciaController::class, 'create']);
+Route::post ('/Dependencias/update', [DependenciaController::class, 'update']);
+Route::delete ('/Dependencias/{id}', [DependenciaController::class, 'delete']);
+
+// api de Usuarios
+Route::get ('/Usuarios', [UsuarioController::class, 'list']);
+Route::get ('/Usuarios/{id}', [UsuarioController::class, 'item']);
+Route::post ('/Usuarios/create', [UsuarioController::class, 'create']);
+Route::post ('/Usuarios/update', [UsuarioController::class, 'update']);
+Route::delete ('/Usuarios/{id}', [UsuarioController::class, 'delete']);
