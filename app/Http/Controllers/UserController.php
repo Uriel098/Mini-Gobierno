2e9be3a30a1c218cb0c5d9a503c3e4d9cb134ebf
Uriel098 <a22311064@uthermosiilo.edu.mx>
2024-04-25 22:49:10 -0700

    UserController Complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,60 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('users.users');
+    }
+
+    public function show($id)
+    {
+        return view('users.show');
+    }
+
+    public function UsersCrear()
+    {
+        return view('users.createusers');
+    }
+
+    public function UserCreate(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->telefono = $request->telefono;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->ciudad = $request->ciudad;
+        $user->curp = $request->curp;
+        $user->save();
+
+        return redirect()->route('users.users');
+    }
+
+    public function UsersEditar($id)
+    {
+        $user = User::find($id);
+        return view('users.EditUsers', compact('user'));
+    }
+
+    public function UsersEdit(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->telefono = $request->telefono;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->ciudad = $request->ciudad;
+        $user->curp = $request->curp;
+        $user->save();
+
+        return redirect()->route('users.users');
+    }
+
+    public function UsersDelete($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->route('users.users');
+    }
 }
